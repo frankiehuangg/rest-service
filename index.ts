8c7eaa535e730275d453b30fdcae54f027848f22
frankiehuangg <13521092@std.stei.itb.ac.id>
2023-11-15 14:46:43 +0700

    fix: merge conflict

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 import express from 'express'
 import cors from 'cors'
+import bodyParser from 'body-parser';
+import prisma from './src/prisma'
 import handlerWrapperError from './src/utils/handlerWrapperError';
 import loginHandler from './src/handler/auth/login';
 import registerHandler from './src/handler/auth/register';
@@ -18,10 +20,13 @@ const startDate = new Date();
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
     console.log(`using database URL ${DB_URL}`);
-})
+});
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (_, res) => {
     res.send(`Server has started since ${startDate}`);
